f5ba6632372a789bf4b915183652ff51223f970b
Lucas <lucasmisidoro@gmail.com>
2018-09-05 20:26:43 -0300

    Aplicando de css nos componentes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -24,6 +24,12 @@ export class HomePage {
   data: string;
   categoria: string;
 
+  showPalestras(): void{
+ /*  palestrasEminicursos = [{titulo: 'Testes 1', corpo: 'Lorem ipsum teste Lorem ipsum teste Lorem ipsum teste ', horario: '12:30', sala: 10},
+    {titulo: 'Testes 2', corpo: 'Lorem ipsum teste Lorem ipsum teste Lorem ipsum teste ', horario: '12:30', sala: 10}, 
+    {titulo: 'Testes 3', corpo: 'Lorem ipsum teste Lorem ipsum teste Lorem ipsum teste ', horario: '12:30', sala: 10}];*/
+  }
+
   constructor(public navCtrl: NavController, public loadingCtrl: LoadingController) {
     const loader = this.loadingCtrl.create({
       content: "Iniciando...",
