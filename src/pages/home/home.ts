1696d8dc4f5b9a2b02e2c45cb8efb2cc0e1d9a9d
HigorRodrigues <higor.italva@gmail.com>
2018-09-19 19:06:51 -0300

    Insere carregamento ao clicar no dia

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,11 +6,9 @@ import {SessaoTecnicaPage} from '../sessao-tecnica/sessao-tecnica';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
-
-                                                                                                                              
-  
+  templateUrl: 'home.html'                                                                                                                            
 })
+
 export class HomePage {
 
   lista = [];
@@ -36,22 +34,42 @@ export class HomePage {
       corpo: 'Mônica Santana Vianna - Pós-Doutoranda do IPRJ/UERJ - João Paulo Torres – Professor da UFRJ', horario: '', sala: ''}
     ];
 
-    dia8 = [{titulo: 'Credenciamento', corpo:'Credenciamento do XXI ENMC e IX ECTM', horario:'8h - 12h30', sala: ''},
-    { titulo: 'Cerimônia de Abertura', corpo: 'Cerimônia de Abertura do ' + 
-    'XXI Encontro Nacional de Modelagem Computacional e IX Encontro de Ciência e Tecnologia dos Materiais', horario: '14h - 14:30', sala: '' },
-    { titulo: 'Coffee Break', corpo: '', horario: '15h30 - 16h', sala: '' },
-    { titulo: 'Painel 1', corpo: '“As aplicações da Modelagem Computacional na Física”', horario: '15h30 - 16h', sala: '' },
-    { titulo: 'Coquetel de Boas Vindas', corpo: 'Coquitel de Boas Vindas para os participantes do XXI ENMC e IX ECTM', horario: '12:30', sala: '' }];
+    dia8 = [
+      {tipo: 'minicurso', titulo: 'Minicurso 1: Introdução ao Uso do MOHID Studio para Estudo Hidrológico', instrutor: 'Jader Lugon Júnior – Professor do IFFluminense', horaInicio: '08:00', horaFim: '10:00'},
+      {tipo: 'minicurso', titulo: 'Minicurso 2: SciLab: Introdução e Aplicação', instrutor: 'Fábio Freitas Ferreira – Professor da UFF', horaInicio: '10:00', horaFim: '11:00'},
+      {tipo: 'geral', titulo: 'Credenciamento', horaInicio: '11:00', horaFim: '12:30'},
+      {tipo: 'geral', titulo: 'Almoço', horaInicio: '12:30', horaFim: '14:00'},
+      {tipo: 'geral', titulo: 'Cerimônia de abertura', horaInicio: '14:00', horaFim: '14:30'},
+      {tipo: 'plESes', titulo: 'Palestra de Abertura: Desafios da Ciência', instrutor: 'Rodolfo Jardim de Azevedo – Diretor do Instituto de Computação da Unicamp', horaInicio: '14:30', horaFim: '15:30'},
+      {tipo: 'geral', titulo: 'Coffee Break - Painel I', horaInicio: '15:30', horaFim: '16:00'},
+      {tipo: 'plESes', titulo: 'Palestra em Modelagem Computacional: Modelagem Computacional do Comportamento dos Tecidos do Corpo Humano', instrutor: 'Aura Conci – Professora da UFF', horaInicio: '16:00', horaFim: '17:00'},
+      {tipo: 'plESes', titulo: 'Palestra em Ciência e Tecnologia de Materiais: Modelagem Numérica de Falha de Materiais: O Caso da Propagação Estável de Trincas', instrutor: 'Eduardo Atem de Carvalho – Professor da UENF', horaInicio: '17:00', horaFim: '18:00'},
+      {tipo: 'geral', titulo: 'Coquetel de Boas Vindas', horaInicio: '18:30', horaFim: '20:00'},      
+    ];
 
-    dia9 = [ { titulo: 'Sessão Pôsteres', corpo: 'Trabalhos de pesquisa submetidos.', horario: '11h - 12h30', sala:  ''},
-    { titulo: 'Coffee Break', corpo: ' ', horario: '15:30 - 16h', sala: ''},
-    { titulo: 'Painel 2', corpo: '“Barco Movido a Energia Solar, o Barco Bismarck Solaris”', horario: '15:30 - 16h', sala:  '' },
-    { titulo: 'Sessão Pôsteres', corpo: 'Trabalhos de pesquisa submetidos.', horario: '15:30 - 16h', sala:  ''}, {titulo: 'Reunião dos Coordenadores de PPG', corpo: '', horario: '17h - 18h', sala: ''}];
+    dia9 = [ 
+      {tipo: 'plESes', titulo: 'Palestra em Sessão Técnica: Espectroscopia Fotoacústica: Breve Introdução e Algumas Aplicações', instrutor: 'Noberto Cella – Professor do IPRJ/UERJ', horaInicio: '08:00', horaFim: '10:00'},
+      {tipo: 'plESes', titulo: 'Palestra 1: Polo de Inovação do IFFluminense no Modelo Embrapii: como a Modelagem Computacional e a Tecnologia dos Materiais podem contribuir na execução dos projetos', instrutor: 'Vicente de Paulo Santos de Oliveira – Pró-Reitor de Pesquisa, Extensão e Inovação do IFFluminense', horaInicio: '10:00', horaFim: '11:00'},
+      {tipo: 'plESes', titulo: 'Sessões técnica e Sessão Pôsteres', horaInicio: '11:00', horaFim: '12:30'},
+      {tipo: 'geral', titulo: 'Almoço', horaInicio: '12:30', horaFim: '14:00'},
+      {tipo: 'plESes', titulo: 'Sessão Técnica e Palestra de Sessão', horaInicio: '14:00', horaFim: '15:30'},
+      {tipo: 'geral', titulo: 'Coffee Break - Painel II - Sessão Pôsteres', horaInicio: '15:30', horaFim: '16:00'},
+      {tipo: 'plESes', titulo: 'Palestra 2: MDO: An Outstanding Methodology For Aircraft Design', instrutor: 'Ana Paula Curty Cuco – Engenheira de Desenvolvimento de Produto da Embraer', horaInicio: '16:00', horaFim: '17:00'},
+      {tipo: 'plESes', titulo: 'Sessão Técnica - Reunião dos Coordenadores de PPG', horaInicio: '17:00', horaFim: '18:00'},
+      {tipo: 'geral', titulo: '*Jantar por Adesão', horaInicio: '18:30', horaFim: '20:00'}
+    ];
 
-    dia10 = [{ titulo: 'Sessão Pôsteres', corpo: 'Trabalhos de pesquisa submetidos.', horario: '11h - 12h30', sala:  ''},
-    { titulo: 'Coffee Break', corpo: ' ', horario: '15:30 - 16h', sala: ''},
-    { titulo: 'Sessão Pôsteres', corpo: 'Trabalhos de pesquisa submetidos.', horario: '15:30 - 16h', sala:  ''},
-    {titulo: 'Cerimônia de Encerramento', corpo: 'Cerimônia de Encerramento do XXI Encontro Nacional de Modelagem Computacional e IX Encontro de Ciência e Tecnologia dos Materiais', horario: '18h30 - 20h', sala: ''}];
+    dia10 = [
+      {tipo: 'plESes', titulo: 'Palestra em Sessão Técnica: Modelagem (Agro)ambiental: Avanços e Urgências nos Estudos sobre Agrotóxicos', instrutor: 'Mônica Santana Vianna - Pós-Doutoranda do IPRJ/UERJ e João Paulo Torres – Professor da UFRJ', horaInicio: '08:00', horaFim: '10:00'},
+      {tipo: 'plESes', titulo: 'Palestra 3: O Algoritmo de Colisão de Partículas (PCA)', instrutor: 'Wagner Figueiredo Sacco – Professor da UFOPA', horaInicio: '10:00', horaFim: '11:00'},
+      {tipo: 'plESes', titulo: 'Sessões técnica e Sessão Pôsteres', horaInicio: '11:00', horaFim: '12:30'},
+      {tipo: 'geral', titulo: 'Almoço', horaInicio: '12:30', horaFim: '14:00'},
+      {tipo: 'plESes', titulo: 'Sessão Técnica e Palestra de Sessão', horaInicio: '14:00', horaFim: '15:30'},
+      {tipo: 'geral', titulo: 'Coffee Break - Sessão Pôsteres', horaInicio: '15:30', horaFim: '16:00'},
+      {tipo: 'plESes', titulo: 'Palestra 4: Problemas Inversos em Dinâmica e Vibrações: Modelagem e Estimação de Parâmetros', instrutor: 'Leonardo Tavares Stutz – Professor do IPRJ/UERJ', horaInicio: '16:00', horaFim: '17:00'},
+      {tipo: 'plESes', titulo: 'Palestra 5: Análise Multicritério', instrutor: 'Helder Gomes Costa – Professor da UFF', horaInicio: '17:00', horaFim: '18:00'},
+      {tipo: 'geral', titulo: 'Cerimônia de Encerramento', horaInicio: '18:30', horaFim: '20:00'}
+    ];
 
  /// select = ["Opcao1", "Opcao2", "Opcao3", "Opcao4", "Opcao5", "Opcao6"];
 
@@ -60,7 +78,7 @@ export class HomePage {
 
   constructor(public navCtrl: NavController, public loadingCtrl: LoadingController) {
 
-    this.carregamento("Iniciando", 3000);
+   
   }
 
 /*  geralPage(): void{
@@ -78,18 +96,24 @@ export class HomePage {
 
 
  
-  carregamento( mensagem, tempo ){
-    const loader = this.loadingCtrl.create({
-      content: mensagem + "...",
-      duration: tempo
-    });
-    loader.present();
-  }
-
   limparLista(event) {
-    this.lista = [];
+    if( this.data == "dia8"){
+      this.lista = this.dia8;
+    }
+    if( this.data == "dia9"){
+      this.lista = this.dia9;
+    }
+    if( this.data == "dia10"){
+      this.lista = this.dia10;
+    }
   }
 
+  retornoTipo(lista, tipo){
+    tipo = [];
+    for( var l in lista){
+      
+    }
+  }
   geral(event){
     if (this.data == "dia8") {
       this.navCtrl.push(GeralPage, {lista: this.dia8});
@@ -103,7 +127,6 @@ export class HomePage {
   }
 
   palestrasEminucursos(event) {
-    this.carregamento("Carregando", 2000);
     if (this.data == "dia8") {
       this.navCtrl.push(PalestrasMinicursosPage, {lista: this.palestrasEminicursosD8});
       //this.lista = this.palestrasEminicursosD8;
@@ -119,10 +142,7 @@ export class HomePage {
     }
   }
 
-  
-
-  sessoesTecnicas(event) {    
-    this.carregamento("Carregando", 2000);
+  sessoesTecnicas(event) {        
     if (this.data == "dia8") {
       this.lista = [];
       this.navCtrl.push(SessaoTecnicaPage);
@@ -135,6 +155,10 @@ export class HomePage {
       this.navCtrl.push(SessaoTecnicaPage, {lista: this.sessaotecnicaD10});
       //this.lista = this.sessaotecnicaD10;
     }
+  }
+
+  emitMessage(){
+
   }
 
   selectCategoria($event, cat) {
